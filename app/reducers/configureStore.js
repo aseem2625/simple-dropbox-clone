f733472190960b5075886f12237090381ab25500
aseem <aseem.g@stayzilla.com>
2017-02-13 07:19:43 +0530

    Remove DevTools completely

--- app/reducers/configureStore.js
+++ app/reducers/configureStore.js
@@ -1,12 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
-// import DevTools from '../containers/DevTools';
 
 export default function configureStore(initialState) {
     const store = createStore(
         rootReducer,
-        initialState,
-        // DevTools.instrument()
+        initialState
     );
 
     return store;
